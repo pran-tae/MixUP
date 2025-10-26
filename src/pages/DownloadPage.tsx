ed9fc44d288c8c4d2c21292fd0d2b9d9c9e540ed
Olivia Hu <oliviahu2020@gmail.com>
2025-10-26 14:04:32 -0400

    implement download page

--- src/pages/DownloadPage.tsx
+++ src/pages/DownloadPage.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import button from "../assets/download_button.png";
+import Logo from "../components/Logo.tsx";
 
 export default function Download() {
   const navigate = useNavigate();
@@ -8,10 +10,17 @@ export default function Download() {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button className="text-4xl p-4" onClick={handleClick}>
-        Back to home
-      </button>
+    <div className="flex items-center pt-16 h-screen flex-col ">
+      <Logo />
+      <div className="flex items-center justify-center flex-col h-full gap-16">
+        <a href="" download className="cursor-pointer" onClick={handleClick}>
+          <img src={button} className="-mb-16 w-lg" />
+        </a>
+        <div className="text-center text-2xl flex flex-col gap-12">
+          <p>Your mix is ready!</p>
+          <p>Tap the button to download</p>
+        </div>
+      </div>
     </div>
   );
 }
